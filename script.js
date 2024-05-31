31d7922447459247507c236988b8fbfa9aaf60c9
sandro <sandrodzidziguri51@gmail.com>
2024-05-31 10:53:18 +0400

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(`hello world`)
+
+let number = 3;
+
+console.log(number)
+
+let number1 = 4;
+
+console.log( number + number1);
+
+console.log( number - number1);
+
+console.log(number / number1);
+
+console.log('firstLine\nsecondLine')
+
+console.log('con'+ 'catenate')
+{
+    let x = 25;
+    console.log(x)
+
+
+}
+console.log(`${100 / 2} is wrong number`)
+
+console.log( 332 % 3 )
+
+
+
+
+
+
