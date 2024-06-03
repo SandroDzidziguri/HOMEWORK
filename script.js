52a711dc6b57593d97a7094491ef16419dc176ee
sandro <sandrodzidziguri51@gmail.com>
2024-06-03 14:29:35 +0400

    first commit

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ console.log(`${100 / 2} is wrong number`)
 console.log( 332 % 3 )
 
 
+var str = "striqoni";
+
+var upperCaseStr = str.toUpperCase();
+
+console.log("didi asoebit: " + upperCaseStr);
+
+var str1 = "first ";
+var str2 = "sentence";
+var combinedStr = str1.concat(str2);
+
+console.log("შედეგი: " + combinedStr);
+
 
 
 
