d71135463081e275ffbfd32c16a4cd6de7738ac6
sandro <sandrodzidziguri51@gmail.com>
2024-06-03 15:27:56 +0400

    second homework

---
+++ script1.js
@@ -0,0 +1,33 @@
+let num = -4
+
+if (  num % 2 == 0) { console.log(true)
+    
+} else {console.log(false)
+    
+}
+
+if (num < 0) {console.log("negative")
+    
+}
+if(num == 0 ) {console.log("equals")
+
+
+}
+     else {console.log("positive")
+    
+}
+
+var str = "";
+
+
+if ( str .length == 0) {console.log("carieli")
+
+    
+} else {console.log("savse")
+    
+}
+
+console.log(Math.min (4, 5))
+
+
+
