f4f1d7b4c39b0bc3096c5bfd1aaa226f09ce6ee4
qaquca <sandrodzidziguri51@gmail.com>
2024-06-13 22:32:57 +0400

    new commit script3

--- script2.js
+++ script2.js
@@ -1,5 +1,50 @@
+// 1 //
+console.log ("hello " + (prompt("whats your name")))
 
-{let name = prompt("whats your name");
+// 2 //
 
-console.log("hello" + name);
+var string = prompt( "enter your word");
+var charCount = string.length;
+console.log(" your string has " + charCount + " letters")
+
+// 3 //
+
+var quote =(prompt( "whats your quote"))
+var author = (prompt("who said it"))
+console.log(  author + " said that " + quote)
+
+// 4 //
+
+var word = prompt( " say it ");
+var lowercase = word.toLowerCase();
+console.log( lowercase )
+
+// 5 //
+
+var sentence = prompt( "enter something");
+var formattedSentence = sentence.replace(/\s+/g, "...")
+console.log (sentence)
+console.log(formattedSentence)
+
+// 6 //
+
+function convert(str) {
+    return str.replace(/:\)/g, '🙂').replace(/:\(/g, '🙁');
 }
+
+function main() {
+    var inputString = prompt("Please enter a sentence:");
+
+    if (inputString !== null && inputString !== "") {
+        var convertedString = convert(inputString);
+
+        console.log('Original: "' + inputString + '"');
+        console.log('Converted: "' + convertedString + '"');
+    } else {
+        console.log("No input provided.");
+    }
+}
+main()
+
+// 7 // 
+
---
+++ script3.js
@@ -0,0 +1,24 @@
+for (let i = 0; i <= 10; i++) {
+    
+    console.log(i)
+}
+
+for (let evenNumber = 0; evenNumber < 20; evenNumber+= 2) {
+    
+    console.log(evenNumber)
+}
+
+const number = parseInt(3)
+for (let ricxv = 0; ricxv < number; ricxv++) {
+    
+    console.log("Hello")
+    
+}
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+
+console.log("რიცხვების ჯამი 1-დან 100-მდე არის: " + sum);
