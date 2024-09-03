2f3459427ceec9e88050dbc9ee74519840dd4a43
sandro dzidziguri <sandrodzidziguri51@gmail.com>
2024-09-03 15:10:12 +0400

    added deep.js

---
+++ deep.js
@@ -0,0 +1,6 @@
+let universe = input("what is the answer of a great question of life, universe, and everything?  ");
+universe.toLowerCase().trim()
+if(universe == "42"){console.log("yes")}
+ else if(universe =="forty two"){console.log("yes")}
+ else if (universe =="forty-two"){console.log("yes")}
+ else {console.log("no")};
