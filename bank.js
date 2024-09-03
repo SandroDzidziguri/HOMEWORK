c385b17142ace9055635e33ba6418f9c7532c013
sandro dzidziguri <sandrodzidziguri51@gmail.com>
2024-09-03 15:40:12 +0400

    added bankjs

---
+++ bank.js
@@ -0,0 +1,8 @@
+
+let greeting = input("say hello to my little friend ");
+
+
+
+if(greeting.toLowerCase().startsWith("hello")){console.log("0$")}
+else if (greeting.toLowerCase().startsWith("h")){console.log("20$")}
+else{console.log("100$")};
