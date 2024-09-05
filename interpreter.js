08aef8402ed41e03ddec179fb95fe119beeff1fb
sandro dzidziguri <sandrodzidziguri51@gmail.com>
2024-09-05 13:15:01 +0400

    added interprete.js

---
+++ interpreter.js
@@ -0,0 +1,15 @@
+let num1 = Number(input("number 1 :"));
+ let exp = input("expression :");
+  let num2 = Number(input("number 2 :"));
+let x = num1;
+ let y = exp;
+  let z = num2;
+if (y == "+") 
+{   print((x + z).toFixed(1));
+ } else if (y == "-") 
+ {   print((x - z).toFixed(1));
+ } else if (y == "*")
+  {   print((x * z).toFixed(1)); 
+  }else if (y == "/") 
+   {   print((x / z).toFixed(1)); 
+  }
