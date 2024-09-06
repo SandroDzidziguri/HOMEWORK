f49cca269b5f041a389f9fd4b84baa99eb57741e
sandro dzidziguri <sandrodzidziguri51@gmail.com>
2024-09-06 15:18:01 +0400

    added coke.js

---
+++ coke.js
@@ -0,0 +1,11 @@
+let coke = input("you need to pay 50 cent to buy coke  ");
+
+if(coke == 50){
+  console.log("here is your coke ");
+}else if (coke == 25){
+  console.log("Amount Due:25 please enter another 25 "); 
+}else if (coke == 10){
+  console.log("Amount Due: 40  please enter another 40");
+}else if (coke == 5 ){
+  console.log("Amount Due: 45 please enter another 45");
+}else ("");
