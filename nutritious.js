92a8d6c068eb582e074fb0e004720be4a8fb4c5c
sandro dzidziguri <sandrodzidziguri51@gmail.com>
2024-09-07 14:02:29 +0400

    added nutritious.js

---
+++ nutritious.js
@@ -0,0 +1,13 @@
+let fruit = input(" enter your fruit ");
+function calculateCalories(fruit){
+  fruit = fruit.toLowerCase();
+  if(fruit = "apple"){
+    console.log("130 calories")
+  }else if ("avocado"){
+    console.log(" 50 calories");
+  }else if (fruit = "sweet cherries"){
+    console.log("100 calories")
+  }else {console.log("")
+}
+}
+calculateCalories(fruit)
